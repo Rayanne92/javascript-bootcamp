48b369fcfc889c5abf5b8d983a7fb3430529d963
Rayanne <103422010+Rayanne92@users.noreply.github.com>
2024-06-03 20:18:13 +0200

    Delete javascript-bootcamp directory

--- javascript-bootcamp/js/motLePlusLong.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// 1) Je récupère mon texte contenu dans le paragraphe
-let phrase = document.getElementById("synopsis").innerText;
-console.log(phrase);
-
-// 2)
--- javascript-bootcamp/js/tirage-sort.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let heros = ["Naruto", "Ichigo", "Izuku", "Tanjiro", "Itadori", "Luffy", "Goku", "Kenshiro", "Ranma", "Seiya", "Mob", "Saitama"];
-
-function versus() {
-
-    document.getElementById("affichage").innerHTML += `<span>${heros} vs. ${heros}</span>`;
-
-}
